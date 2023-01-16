ff7698ec22c97b1868a33240988b56589a389a73
anais-labit <anais.labit@gmail.com>
2023-01-16 12:31:22 +0100

    job5 - boucle for

---
+++ jour01/job05/script.js
@@ -0,0 +1,17 @@
+const semaines = [
+  "lundi",
+  "mardi",
+  "mercredi",
+  "jeudi",
+  "vendredi",
+  "samedi",
+  "dimanche",
+];
+
+function afficherjourssemaines() {
+  for (const jour of semaines) {
+    console.log (jour);
+  }
+}
+
+console.log(afficherjourssemaines());
