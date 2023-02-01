b9dea68cd982c9846c997d07247ee4eb9c617f23
anais-labit <anais.labit@gmail.com>
2023-02-01 11:23:39 +0100

    découverte de jQuery

---
+++ jour03/job01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <title>Job01 jour3</title>
+</head>
+
+<body>
+    <button id="hide">Hide</button>
+    <button id="show">Show</button>
+    <p>“Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie.”</p>
+</body>
+
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,12 @@
+// $("#hide").click(function () {
+//   $("p").hide();
+// });
+
+$("#hide").on("click", () => {
+  $("p").hide();
+});
+
+$("#show").on("click", () => {
+  $("p").show();
+});
+
